59685ffd79df389c6ee42d23c9ff480313bb3db0
AlinaSydoruk <sidorukalinasev2020@gmail.com>
2024-04-14 23:23:59 +0200

    init

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import redis
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add_word', methods=['POST'])
+def add_word():
+    word = request.form['word']
+    translation = request.form['translation']
+    cache.set(word, translation)
+    print(f"Added word: {word}, Translation: {translation}")
+    return render_template('add_word_result.html')
+
+@app.route('/translate', methods=['GET', 'POST'])
+def translate():
+    if request.method == 'POST':
+        word = request.form['word']
+        translation = cache.get(word)
+        if translation:
+            translation = translation.decode('utf-8')  # Декодуємо байтовий рядок в рядок Unicode
+            return render_template('result.html', word=word, translation=translation)
+        else:
+            return render_template('result.html', word=word, translation="Translation not found")
+
+if __name__ == '__main__':
+    app.run(debug=True)
