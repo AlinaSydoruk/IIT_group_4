661d4457f7103405e52061315563c550d6c9a3c7
Valeriia Khyzhniak <valeria.khyzhniak21@gmail.com>
2024-04-16 19:52:15 +0200

    1

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request
 import redis
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+try:
+    cache = redis.Redis(host='redis', port=6379)
+except Exception as e:
+    print(f"Error connecting to Redis: {e}")
+    cache = None
 
 @app.route('/')
 def index():
@@ -10,22 +14,34 @@ def index():
 
 @app.route('/add_word', methods=['POST'])
 def add_word():
-    word = request.form['word2']
-    translation = request.form['translation']
-    cache.set(word, translation)
-    print(f"Added word: {word}, Translation: {translation}")
-    return render_template('add_word_result.html')
+    try:
+        word = request.form['word2']
+        translation = request.form['translation']
+        if cache:
+            cache.set(word, translation)
+            print(f"Added word: {word}, Translation: {translation}")
+            return render_template('add_word_result.html')
+        else:
+            return "Error: Cache is not available"
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 @app.route('/translate', methods=['GET', 'POST'])
 def translate():
     if request.method == 'POST':
-        word = request.form['word1']
-        translation = cache.get(word)
-        if translation:
-            translation = translation.decode('utf-8')  # Декодуємо байтовий рядок в рядок Unicode
-            return render_template('result.html', word1=word, translation=translation)
-        else:
-            return render_template('result.html', word1=word, translation="Translation not found")
+        try:
+            word = request.form['word1']
+            if cache:
+                translation = cache.get(word)
+                if translation:
+                    translation = translation.decode('utf-8')  # Декодуємо байтовий рядок в рядок Unicode
+                    return render_template('result.html', word=word, translation=translation)
+                else:
+                    return render_template('result.html', word=word, translation="Translation not found")
+            else:
+                return "Error: Cache is not available"
+        except Exception as e:
+            return f"An error occurred: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
